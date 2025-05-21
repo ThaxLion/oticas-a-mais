48ceb824fba49bcdc8226c002ff1fc3f930ec5a6
ThaxLion <benjaminguilherme38@gmail.com>
2025-05-20 23:00:06 -0300

    Add files via upload

--- carrinho.js
+++ carrinho.js
@@ -1,13 +1,5 @@
-function adicionarAoCarrinho() {
-  let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-  carrinho.push("Óculos");
-  localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  atualizarContador();
-}
 
-function atualizarContador() {
-  const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-  document.getElementById("contador-carrinho").innerText = carrinho.length;
-}
-
-document.addEventListener("DOMContentLoaded", atualizarContador);
+document.addEventListener("DOMContentLoaded", function () {
+    const contador = document.getElementById("contador-carrinho");
+    contador.textContent = localStorage.getItem("carrinho") || "0";
+});
